f0514555aa9d3f2f40e4b2db8f09e466fbea84e7
ShridharMP <mpshridhar2016@gmail.com>
2020-08-04 02:39:38 -0400

    Git commit for Jenkin build

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+public static void main(String[] args)
+{
+	System.out.println("Hi Welcome to Linux Tutorial commiting the Code in Github and Building it in Jenkins");
+}
+}
