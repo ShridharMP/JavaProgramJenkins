1c92faa23b2f432a601abede236200862f59e2c8
ShridharMP <mpshridhar2016@gmail.com>
2024-07-28 17:59:11 +0530

    Recommit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-public class HelloWorld{
-public static void main(String[] args)
-{
-	System.out.println("Hi Welcome to Linux Tutorial commiting the Code in "+
-	"Github and Building it in Jenkins");
-}
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hi Welcome to Linux Tutorial commiting the Code in " +
+				"Github and Building it in Jenkins");
+
+		System.out.println("Jenkins Learning restarted");
+	}
 }
